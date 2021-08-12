28ebb806024284202e38c80aabca0a2687bc6333
Amirhossein Saemi <saemiamirhossein@gmail.com>
2021-08-12 13:11:13 -0400

    Enhanced the security of shorten function by adding more salt :)

--- the_shortest_url/shortener/utils.py
+++ the_shortest_url/shortener/utils.py
@@ -2,6 +2,7 @@ import hashlib
 import time
 from random import choice
 from typing import Union
+from string import ascii_lowercase, ascii_uppercase
 
 from django.conf import settings
 from django.core.cache import cache
@@ -12,35 +13,39 @@ from . import exceptions
 
 def shorten(
         url: str,
-        length: int = settings.SHORTENER_LENGTH
+        length: int = settings.SHORTENER_LENGTH,
+        with_salt: bool = True
 ) -> str:
 
     """
     returns the shortened version of the url in the requested size.
-    calling this function with same urls does not return the same outputs.
-    A hash function is used in this function instead of randomly generating a key for each url.
-    The reason is that a hash function (without a salt) could be used to generate the same key for the same url
+    calling this function with same urls does not return the same outputs unless with_salt is False.
+    A hash function is used instead of randomly generating a key for each url.
+    The reason is that a hash function (without a salt) could be used to re-generate the same key for the same url
     which can be useful if we need such a feature.
     Also, great Linus Torvalds has used a hash function as a key in his great project: GIT
     """
 
-    # a couple of things are mixed together in order to create unique keys
+    # a couple of things are mixed together in order to create a unique key
     # for each url and avoid collisions.
     _hash = hashlib.sha1()
-    for item in [
-        str(time.time_ns()),
-        url
-    ]:
-        # update will just concatenate the items together.
-        # https://docs.python.org/3/library/hashlib.html#hashlib.hash.update
-        _hash.update(item.encode())
+    _hash.update(url.encode())
+
+    if with_salt:
+        for item in [
+            str(time.time_ns()),
+            "".join(choice([ascii_lowercase, ascii_uppercase]) for i in range(20))
+        ]:
+            # update will just concatenate the items together.
+            # https://docs.python.org/3/library/hashlib.html#hashlib.hash.update
+            _hash.update(item.encode())
 
     key = _hash.hexdigest()[-length:]
     # the output of a hash function is always lowercase,
     # so to generate a key which has both uppercase and lowercase letters
     # we change some characters to uppercase ones randomly.
     # this will decrease the chance of collision. Although a collision is
-    # very rare because of the mixture we created above.
+    # very rare because of the mixture we created above when with_salt = True.
     return ''.join(
         choice((str.upper, str.lower))(char) for char in key  # noqa
     )
